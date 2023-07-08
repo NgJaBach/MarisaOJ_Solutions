47ceed59ebeb3a85c5f034773892daf77917470a
Nguyễn Gia Bách <ngbach2008@gmail.com>
2023-07-09 00:07:25 +0700

    Add files via upload

---
+++ level0/IO_Condition/distance.cpp
@@ -0,0 +1,58 @@
+// NgJaBach: Forever Meadow <3
+
+#include<bits/stdc++.h>
+
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef long long int ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define upb upper_bound
+#define lwb lower_bound
+#define bend(a) a.begin(),a.end()
+#define rev(x) reverse(bend(x))
+#define mset(a) memset(a, 0, sizeof(a))
+#define fi first
+#define se second
+#define gcd __gcd
+#define getl(s) getline(cin, s);
+#define setpre(x) fixed << setprecision(x)
+#define endl '\n'
+const int N=200050,M=1000000007;
+const ll INF=1e18+7;
+double cal(pair<double,double>A,pair<double,double>B){
+    A.fi-=B.fi;
+    A.se-=B.se;
+    return sqrt(A.fi*A.fi+A.se*A.se);
+}
+pair<double,double>input(){
+    ll a,b;
+    cin>>a>>b;
+    return {a,b};
+}
+int main(){
+    ios_base::sync_with_stdio(NULL); cin.tie(nullptr); cout.tie(nullptr);
+//    freopen(".inp","r",stdin);
+//    freopen(".out","w",stdout);
+    pair<double,double>A,B;
+    A=input();
+    B=input();
+    cout<<setpre(2)<<cal(A,B);
+    return 0;
+}
+/*
+==================================+
+INPUT:                            |
+------------------------------    |
+
+------------------------------    |
+==================================+
+OUTPUT:                           |
+------------------------------    |
+
+------------------------------    |
+==================================+
+*/
+
