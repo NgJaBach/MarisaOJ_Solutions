6ff0db70880417fd2077bdf67f51ad939da1e3f1
Nguyễn Gia Bách <ngbach2008@gmail.com>
2023-08-06 23:28:03 +0700

    Add files via upload

--- level1/backtracking/knight_tour.cpp
+++ level1/backtracking/knight_tour.cpp
@@ -20,32 +20,21 @@ typedef unsigned long long ull;
 #define getl(s) getline(cin, s);
 #define setpre(x) fixed << setprecision(x)
 #define endl '\n'
-const int N=10,M=1000000007;
+const int N=8,M=1000000007;
 const ll INF=1e18+7;
-int table[N][N],n,m;
-void tableclear(){
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=m;++j){
-            table[i][j]=0;
-        }
-    }
-    return;
-}
-void tableout(){
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=m;++j){
-            cout<<table[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    return;
-}
-const int dx[]={1,1,-1,-1,2,2,-2,-2};
-const int dy[]={2,-2,2,-2,1,-1,1,-1};
-void dfs(int x,int y,int k){
+const int dx[]={ 2, 1, -1, -2, -2, -1, 1, 2 };
+const int dy[]={ 1, 2, 2, 1, -1, -2, -2, -1 };
+int n,m,table[N][N];
+bool dfs(int x,int y,int k){
+//    cout<<x<<" "<<y<<" "<<k<<endl;
     if(k==n*m){
-        tableout();
-        exit(0);
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=m;++j){
+                cout<<table[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return true;
     }
     ++k;
     int nx,ny;
@@ -55,17 +44,18 @@ void dfs(int x,int y,int k){
         if(nx<1 or ny<1 or nx>n or ny>m) continue;
         if(table[nx][ny]) continue;
         table[nx][ny]=k;
-        dfs(nx,ny,k);
+        if(dfs(nx,ny,k)) return true;
         table[nx][ny]=0;
     }
-    return;
+    return false;
 }
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 //    freopen(".inp","r",stdin);
 //    freopen(".out","w",stdout);
     cin>>n>>m;
-    
+    table[1][1]=1;
+    dfs(1,1,1);
     return 0;
 }
 /*
